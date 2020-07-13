d1b11e9068ac6b9e621fe36925432a6145f550fd
Waldemar Pietruszkiewicz <waldek@suwalki.com.pl>
2020-07-13 18:52:03 +0200

    pierwszy

---
+++ wyciagiCollect/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace wyciagiCollect
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(428, 355);
+            this.panel1.TabIndex = 1;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(55, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Przeciągnij myszką i upuść";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 377);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(457, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 2;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(48, 17);
+            this.toolStripStatusLabel1.Text = "appInfo";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 399);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Wyciągi Collect Pekao S.A.";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ wyciagiCollect/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Xls;
+using static Spire.Xls.Core.Spreadsheet.XlsWorksheet;
+
+namespace wyciagiCollect
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private string dateToDateWithLine(string input) { 
+            return input.Substring(0, 4) + "-" + input.Substring(4, 2) + "-" + input.Substring(6, 2);
+        }
+
+        private void getDataFromFile(string fileName)
+        {
+            string[] allFileLines = File.ReadAllLines(fileName,Encoding.Default);
+            int lineCount = allFileLines.Length;
+            string lastLine = allFileLines[allFileLines.Length-1];
+            string firstLine = allFileLines[0];
+            if (firstLine.Split(',')[0] != "01" || lastLine.Split(',')[0] != "03")
+            {
+                MessageBox.Show("BŁĄD\r\nNiepoprawny format pliku", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Workbook workbook = new Workbook();
+                Worksheet sheet = workbook.Worksheets[0];
+                
+                
+                sheet.Name = "Wyciąg bankowy " + dateToDateWithLine(firstLine.Split(',')[3]);
+                //Writes hello world to A1
+                sheet.Range["A1"].Text = "Plik z Banku: " + Path.GetFileName(fileName);
+                sheet.DefaultRowHeight = 20;
+                string newRange = "A2";
+                int currentRowNumber = 3;
+                newRange = "A" + currentRowNumber.ToString();
+                sheet.Range[newRange].BorderAround(LineStyleType.Thin, ExcelColors.Black);
+                sheet.Range[newRange].VerticalAlignment = VerticalAlignType.Center;
+                sheet.Range[newRange].HorizontalAlignment = HorizontalAlignType.Center;
+                sheet.Range[newRange].Text = "Data wpłaty";
+                newRange = "B" + currentRowNumber.ToString();
+                sheet.Range[newRange].BorderAround(LineStyleType.Thin, ExcelColors.Black);
+                sheet.Range[newRange].VerticalAlignment = VerticalAlignType.Center;
+                sheet.Range[newRange].HorizontalAlignment = HorizontalAlignType.Center;
+                sheet.Range[newRange].Text = "Nr ew.";
+                newRange = "C" + currentRowNumber.ToString();
+                sheet.Range[newRange].BorderAround(LineStyleType.Thin, ExcelColors.Black);
+                sheet.Range[newRange].VerticalAlignment = VerticalAlignType.Center;
+                sheet.Range[newRange].HorizontalAlignment = HorizontalAlignType.Center;
+                sheet.Range[newRange].Text = "Wpłacający";
+                newRange = "D" + currentRowNumber.ToString();
+                sheet.Range[newRange].BorderAround(LineStyleType.Thin, ExcelColors.Black);
+                sheet.Range[newRange].VerticalAlignment = VerticalAlignType.Center;
+                sheet.Range[newRange].HorizontalAlignment = HorizontalAlignType.Center;
+                sheet.Range[newRange].Text = "Kwota";
+                for (int i = 1; i < allFileLines.Length-1; i++)
+                {
+
+                
+                    currentRowNumber = i+3;
+                    newRange = "A" + currentRowNumber.ToString();
+                    sheet.Range[newRange].ColumnWidth = 10;
+                    sheet.Range[newRange].BorderAround(LineStyleType.Thin, ExcelColors.Black);
+                    sheet.Range[newRange].VerticalAlignment = VerticalAlignType.Center;
+                    sheet.Range[newRange].HorizontalAlignment = HorizontalAlignType.Center;
+                    sheet.Range[newRange].Style.Font.Size = 10;
+                    sheet.Range[newRange].Text = dateToDateWithLine(allFileLines[i].Split(',')[5]); //data wpłaty
+                    
+                    newRange = "B" + currentRowNumber.ToString();
+                    sheet.Range[newRange].ColumnWidth = 10;
+                    sheet.Range[newRange].BorderAround(LineStyleType.Thin, ExcelColors.Black);
+                    sheet.Range[newRange].VerticalAlignment = VerticalAlignType.Center;
+                    sheet.Range[newRange].HorizontalAlignment = HorizontalAlignType.Center;
+                    sheet.Range[newRange].Style.Font.Size = 16;
+                    sheet.Range[newRange].Value = allFileLines[i].Split(',')[12].Substring(15, 11); //numer ewidencyjny (końcówka NRB)
+
+                    newRange = "C" + currentRowNumber.ToString();
+                    sheet.Range[newRange].ColumnWidth = 50;
+                    sheet.Range[newRange].BorderAround(LineStyleType.Thin, ExcelColors.Black);
+                    sheet.Range[newRange].VerticalAlignment = VerticalAlignType.Center;
+                    sheet.Range[newRange].HorizontalAlignment = HorizontalAlignType.Left;
+                    sheet.Range[newRange].Style.WrapText = true;
+                    sheet.Range[newRange].Style.Font.Size = 10;
+                    sheet.Range[newRange].Value = allFileLines[i].Split(',')[6]; //wpłacający
+
+                    newRange = "D" + currentRowNumber.ToString();
+                    sheet.Range[newRange].ColumnWidth = 13;
+                    sheet.Range[newRange].Style.NumberFormat = "0.00";
+                    sheet.Range[newRange].BorderAround(LineStyleType.Thin, ExcelColors.Black);
+                    sheet.Range[newRange].VerticalAlignment = VerticalAlignType.Center;
+                    sheet.Range[newRange].HorizontalAlignment = HorizontalAlignType.Right;
+                    sheet.Range[newRange].Style.Font.Size = 16;
+                    sheet.Range[newRange].Value = allFileLines[i].Split(',')[3].Replace('.', ','); //kwota wpłaty
+                }
+                currentRowNumber++;
+                newRange = "C" + currentRowNumber.ToString();
+                sheet.Range[newRange].Text = "Łączna kwota wpłat w pliku:";
+                sheet.Range[newRange].Style.Font.Size = 16;
+                sheet.Range[newRange].Style.Font.Color = System.Drawing.Color.Red;
+                newRange = "D" + currentRowNumber.ToString();
+                sheet.Range[newRange].Style.NumberFormat = "0.00";
+                sheet.Range[newRange].Formula= "=SUM(D4:D"+ (currentRowNumber-1).ToString()+")";
+                sheet.Range[newRange].Style.Font.Size = 16;
+                sheet.Range[newRange].Style.Font.Color = System.Drawing.Color.Red;
+                
+                string fileNameToSave = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Bank_Pliki\" + DateTime.Now.ToString("yyyy-MM-dd_HH.mm") + ".xlsx";
+                workbook.Worksheets[1].Remove();
+                workbook.Worksheets[1].Remove();
+                workbook.SaveToFile(fileNameToSave,ExcelVersion.Version2013);
+                //workbook.PrintDocument.Print();
+
+                try
+                {
+                    System.Diagnostics.Process.Start(workbook.FileName);
+                }
+                catch { }
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            label1.Text= "Przeciągnij myszką i upuść \r\nw tym miejscu plik wynikowy \r\nz Banku";
+            toolStripStatusLabel1.Text = "© 2020 CUW Suwałki v." + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Move;
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            foreach (string path in (string[])e.Data.GetData(DataFormats.FileDrop))
+            {
+                if (Path.GetExtension(path) != ".csv")
+                {
+                    MessageBox.Show("BŁĄD\r\nNiepoprawny format pliku\r\nDopuszczone tylko pliki z rozszerzeniem CSV", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else {
+                    try
+                    {
+                        getDataFromFile(path);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("BŁĄD\r\npodczas przetwarzania pliku z Banku \r\n"+ex.Message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
